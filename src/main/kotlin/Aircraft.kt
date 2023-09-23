c5dfe595723c38c2ac19c3503ac4efde8aebeb3f
semenyutenko <semenyutenko@gmail.com>
2023-09-24 01:00:39 +0300

    task_20

--- src/main/kotlin/Aircraft.kt
+++ src/main/kotlin/Aircraft.kt
@@ -6,6 +6,10 @@ open class Aircraft(val id: Int, val maxRange: Double, val tankCapacity: Double)
         get() = tankCapacity / maxRange * 100
 }
 
-// task_19
-class Boeing747(id: Int, maxRange: Double, tankCapacity: Double, val passengersNumber: Int) :
-    Aircraft(id, maxRange, tankCapacity)
+// task_20
+class Boeing747(id: Int, maxRange: Double, tankCapacity: Double, override val passengersNumber: Int) :
+    Aircraft(id, maxRange, tankCapacity), Passenger
+
+interface Passenger {
+    val passengersNumber: Int
+}
